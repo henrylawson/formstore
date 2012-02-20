bfd7a75356fc26ca57c70650789822417338fd68
Henry Lawson <henry.lawson@foinq.com>
2012-02-20 20:00:56 +0530

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Formstore::Application.config.secret_token = '096a66dfaa9f92140cd681adbda3d16b743386012fb1b134bf1ed1a7ba7bd3d5e44708927fcd9a753f02369d2564973a75ee609ba0249b1b143709a43a572f67'
